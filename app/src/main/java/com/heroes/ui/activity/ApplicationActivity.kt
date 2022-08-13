003b2b4cf1880b41055b88e4887274c3928c7d11
Alon Shlider <benkl15@gmail.com>
2022-08-13 23:15:19 +0300

    moved dashboard to start from root activity

--- app/src/main/java/com/heroes/ui/activity/ApplicationActivity.kt
+++ app/src/main/java/com/heroes/ui/activity/ApplicationActivity.kt
@@ -2,11 +2,17 @@ package com.heroes.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.ui.ExperimentalComposeUiApi
 import com.heroes.R
+import com.heroes.ui.application_flow.dashboard.fragment.DashboardScreen
 
+@ExperimentalComposeUiApi
 class ApplicationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_application)
+        setContent {
+            DashboardScreen()
+        }
     }
 }
