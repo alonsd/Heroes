0dfa7a9e807bf5aa36d84e060ef31d92a07e66f1
Alon Shlider <alon.shlider@gmail.com>
2022-12-18 11:33:17 +0200

    migrated root gradle and settings gradle to kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,11 @@
 buildscript {
     dependencies {
-        classpath "com.android.tools.build:gradle:7.3.1"
-        classpath "androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3"
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21"
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
     }
 } // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.google.devtools.ksp") version "1.7.21-1.0.8"
     id("com.google.dagger.hilt.android") version "2.44" apply false
-}
-task clean(type: Delete) {
-    delete rootProject.buildDir
 }
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+import java.net.URI
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://jitpack.io")
+        }
+    }
+}
+rootProject.name = "Heroes"
+include(":app")
